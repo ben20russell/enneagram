f1b988426fe6f3b73bf5473f94c164aedf1ba9d7
ben20russell <ben20russell@gmail.com>
2026-05-19 19:45:25 -0700

    Favicon update

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,13 @@
 import Providers from "./providers";
 
+export const metadata = {
+  icons: {
+    icon: "/favicon.svg",
+    shortcut: "/favicon.svg",
+    apple: "/favicon.svg",
+  },
+};
+
 export default function RootLayout({ children }) {
   console.log("[layout] rendering RootLayout");
   return (
